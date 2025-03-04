97562e8918ec4e7ae6f5ae71d42d3e5fc73d650f
igormatt0s <yigor88mattos@gmail.com>
2025-03-04 18:40:02 -0300

    feat: Adicionar a função para sortear os amigos

--- app.js
+++ app.js
@@ -3,15 +3,16 @@
 let amigos = []
 
 function adicionarAmigo() {
-    let amigo = document.getElementById('amigo').value
-    if(amigo.trim() === '') {
+    let amigoNome = document.getElementById('amigo').value
+    if(amigoNome.trim() === '') {
         alert('Por favor, insira um nome.')
+    } else if (amigos.includes(amigoNome)) {
+        alert('Este nome já está na lista!')
     } else {
-        amigos.push(amigo)
+        amigos.push(amigoNome)
     }
 
-    amigo = ''
-    console.log(amigos)
+    document.getElementById("amigo").value = ''
 
     let lista = document.getElementById('listaAmigos')
     lista.innerHTML = ''
@@ -20,5 +21,18 @@ function adicionarAmigo() {
         let item = document.createElement('li')
         item.textContent = amigo
         lista.appendChild(item)
+    }
+}
+
+function sortearAmigo() {
+    if(amigos.length > 0) {
+        let amigoEscolhido = parseInt(Math.random() * amigos.length)
+
+        let resultado = document.getElementById('resultado')
+        resultado.innerHTML = ''
+
+        let nomeEscolhido = document.createElement('li')
+        nomeEscolhido.textContent = `O amigo secreto sorteado é: ${amigos[amigoEscolhido]}`
+        resultado.appendChild(nomeEscolhido)
     }
 }
